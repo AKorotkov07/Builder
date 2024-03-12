3a9709fe376526b572857d2f21c16508e495694f
AKorotkov07 <153372700+AKorotkov07@users.noreply.github.com>
2024-03-12 11:43:00 +0300

    Add files via upload

---
+++ src/Person.java
@@ -0,0 +1,76 @@
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age = -1;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age != -1;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return hasAge() ? OptionalInt.of(age) : OptionalInt.empty();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) {
+            age++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + " (" + (hasAge() ? age : "unknown") + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, address);
+    }
+
+    public PersonBuilder newChildBuilder() {
+        PersonBuilder builder = new PersonBuilder();
+        builder.setSurname(surname);
+        if (hasAge()) {
+            builder.setAge(0);
+        }
+        if (hasAddress()) {
+            builder.setAddress(address);
+        }
+        return builder;
+    }
+}
---
+++ src/PersonBuilder.java
@@ -0,0 +1,36 @@
+public class PersonBuilder {
+    private String name;
+    private String surname;
+    private int age = -1;
+    private String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age can't be negative");
+        }
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null) {
+            throw new IllegalStateException("Name and surname are required");
+        }
+        return new Person(name, surname, age);
+    }
+}
